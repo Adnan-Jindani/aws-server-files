a05a3aa325ae4e6f718b98d552ccb19e36df89d4
AAJ227 <88643855+Adnan-Jindani@users.noreply.github.com>
2022-07-08 09:41:31 +0000

    update

--- hello.py
+++ hello.py
@@ -6,17 +6,17 @@ list = []
 guesses = open("guesses.txt", "a")
 
 input = input("Which 3 letter password hash do you want to decrypt?\n")
-data = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, , !, @, #, $, %, ^, &, *, (, ), <, >, :, ;".split(",")
+data = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, , !, @, #, $, %, ^, &, *, (, ), <, >, :, ;, , , , , , , , , , , , , , , , , , , , , 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, {, }, [, ], /, \, ?, |, `, ~".split(",")
 
 b = ""
 bHash = ""
 
 print("\n\nPlease Wait, this process takes some time...")
 
-
-while input != bHash:
+if b not in list:
+  while input != bHash:
     b = (random.choice(data) + random.choice(data) + random.choice(data) + random.choice(data) + random.choice(data) + random.choice(data) + random.choice(data) + random.choice(data) + random.choice(data) + random.choice(data) + random.choice(data) + random.choice(data) + random.choice(data) + random.choice(data) + random.choice(data) + random.choice(data) + random.choice(data) + random.choice(data) + random.choice(data) + random.choice(data)).replace(" ", "")
-    b.strip
+    b.strip()
     bHash = hashlib.sha256(b.encode())
     bHash = bHash.hexdigest()
     print(bHash + "\n")
